1997fb72ea64af246d2696cee628bfa445eecc8d
Stancw <yolo_stance@protonmail.com>
2019-10-25 14:52:41 +0200

    first task done

---
+++ djang/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/stancw/Рабочий стол/100%/django/djang/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ djang/lib/python3.7/weakref.py
@@ -0,0 +1,1 @@
+/home/stancw/.pyenv/versions/3.7.1/lib/python3.7/weakref.py
